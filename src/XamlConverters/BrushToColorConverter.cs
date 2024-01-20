46b2718a98ce8c3d0d3ae0facce2977cad3be0d8
Chris Pulman <chris.pulman@yahoo.com>
2024-01-20 01:04:29 +0000

    Add Comments

--- src/XamlConverters/BrushToColorConverter.cs
+++ src/XamlConverters/BrushToColorConverter.cs
@@ -7,8 +7,22 @@ using System.Windows.Media;
 
 namespace CP.Xaml.Converters;
 
-internal class BrushToColorConverter : IValueConverter
+/// <summary>
+/// BrushToColorConverter.
+/// </summary>
+/// <seealso cref="System.Windows.Data.IValueConverter" />
+public class BrushToColorConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a value.
+    /// </summary>
+    /// <param name="value">The value produced by the binding source.</param>
+    /// <param name="targetType">The type of the binding target property.</param>
+    /// <param name="parameter">The converter parameter to use.</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>
+    /// A converted value. If the method returns null, the valid null value is used.
+    /// </returns>
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => value switch
     {
         SolidColorBrush brush => brush.Color,
@@ -16,5 +30,15 @@ internal class BrushToColorConverter : IValueConverter
         _ => Colors.Red
     };
 
+    /// <summary>
+    /// Converts a value.
+    /// </summary>
+    /// <param name="value">The value that is produced by the binding target.</param>
+    /// <param name="targetType">The type to convert to.</param>
+    /// <param name="parameter">The converter parameter to use.</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>
+    /// A converted value. If the method returns null, the valid null value is used.
+    /// </returns>
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => Binding.DoNothing;
 }
